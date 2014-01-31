c1fd44bb9df6c825e8087d575cab9a5545476ede
Pavel Anossov <anossov@gmail.com>
2014-02-01 03:14:15 +0400

    qi: add Alan Davies to data; exclude Divination from Alan Davies; strip footnotes from episodes names

--- qi/generate_data.py
+++ qi/generate_data.py
@@ -22,7 +22,11 @@ for series in soup.find_all('h3'):
         if len(fields) < 2:
             continue
 
-        episode_title = ''.join(fields[0].stripped_strings).replace('"', '')
+        et = fields[0]
+        if et.find('sup'):
+            et.find('sup').extract()
+
+        episode_title = ''.join(et.stripped_strings).replace('"', '')
         if 'QI VG' in episode_title or 'Compilation' in episode_title:
             continue
 
@@ -39,8 +43,10 @@ for series in soup.find_all('h3'):
             if p and p != 'N/A':
                 panelists[p].append(ep)
 
-
+panelists['Alan Davies'] = [e for e in episodes if e != 'Divination']
 panelist_data = [{'name': k, 'episodes': [episodes.index(e) for e in v]} for k, v in panelists.items()]
+
+episodes.append('L series placeholder')
 
 print 'var EPISODES = {};'.format(json.dumps(episodes, indent=4))
 print 'var PANELISTS = {};'.format(json.dumps(panelist_data, indent=4))
